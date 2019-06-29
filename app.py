b497f0ca200d80b6bb24252390a2de6daab297b2
Kevin Zecchini <kzecchini@Kevins-MBP-3.fios-router.home>
2019-06-29 18:45:38 -0400

    adding ability to update personal voice greeting from youtube clip

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 import random
 import logging
 
-from utils import get_files, clean_message
+from utils import *
 
 log_fmt = "%(asctime)s %(levelname)s %(filename)s %(funcName)s: %(message)s"
 logging.basicConfig(format=log_fmt, level=logging.INFO)
@@ -18,9 +18,10 @@ load_dotenv()
 
 TOKEN = os.environ['TOKEN']
 VOICE_PATH = os.environ['VOICE_PATH']
+DEFAULT_VOICE_PATH = os.environ['DEFAULT_VOICE_PATH']
 GARFIELD_PATH = os.environ['GARFIELD_PATH']
 
-VOICE_FILES = get_files(VOICE_PATH)
+DEFAULT_VOICE_FILES = get_files(DEFAULT_VOICE_PATH)
 GARFIELD_FILES = get_files(GARFIELD_PATH)
 
 TOMMY_PHRASES = [
@@ -45,15 +46,20 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    logging.info("The bot is ready!")
-    # discord.opus.load_opus()
     name = random.choice(TOMMY_PHRASES)
-    logging.info("opus status: {}".format(discord.opus.is_loaded()))
+    logging.info("Opus status: {}".format(discord.opus.is_loaded()))
+    init_db()
+    logging.info("db initialized!")
+    logging.info("The bot is ready!")
     await client.change_presence(status=discord.Status.online,
                                  activity=discord.Game(name=name))
 
 @client.event
 async def on_message(message):
+    # set db connection
+    conn = sqlite3.connect(os.path.join(DATA_PATH, 'discord.db'))
+    c = conn.cursor()
+
     # it's me! do nothing
     if message.author == client.user:
         return
@@ -107,9 +113,37 @@ async def on_message(message):
                 msg = msg.format(message.author.mention, waifu)
                 await message.channel.send(content=msg)
 
+    if message.content.startswith("!clip"):
+        member_id = message.author.id
+        if message.content == "!clip":
+            # set back to default
+            c.execute('''delete from voice_clips where member_id = {}'''.format(member_id))
+            conn.commit()
+            await message.channel.send(content="Removed {}'s soundclip - back do default!".format(message.author.mention))
+        else:
+            try:
+                _, url, start_time, end_time = message.content.split()
+                start_time = convert_time(start_time)
+                end_time = convert_time(end_time)
+                if (end_time - start_time) > MAX_CLIP_LEN:
+                    message.channel.send(content="Clip length too long, must be less than or equal to {}ms".format(MAX_CLIP_LEN))
+                    raise Exception
+                async with message.channel.typing():
+                    path = download_video(url, YDL_OPTS, VOICE_PATH)
+                    clipped_path = split_video(path, start_time, end_time)
+                    c.execute('''replace into voice_clips (member_id, voice_clips) 
+                                values ({}, "{}")'''.format(member_id, clipped_path))
+                    conn.commit()
+                await message.channel.send(content="Updated {}'s voice greeting!".format(message.author.mention))
+            except Exception as e:
+                await message.channel.send(content="Sorry {} - something went wrong in adding your clip!".format(message.author.mention))
+                raise e
 
 @client.event
 async def on_voice_state_update(member, before, after):
+    # set db connection
+    conn = sqlite3.connect(os.path.join(DATA_PATH, 'discord.db'))
+    c = conn.cursor()
     if member.id == client.user.id:
         logging.info("this is me! do nothing")
         return
@@ -123,9 +157,11 @@ async def on_voice_state_update(member, before, after):
             vc = await voice_client.channel.connect()
         # if the member is connecting to this channel from anywhere else - play a greeting!
         if (voice_client.channel == after.channel) and (before.channel != after.channel):            
-            filename = random.choice(VOICE_FILES)
-            logging.info("playing file: {}".format(filename))
-            audio_source = discord.FFmpegPCMAudio(filename)
+            clip_path = get_current_clip(member.id, c)
+            if clip_path is None:
+                clip_path = random.choice(DEFAULT_VOICE_FILES)
+            logging.info("playing file: {}".format(clip_path))
+            audio_source = discord.FFmpegPCMAudio(clip_path)
             # wait for user to connect to hear the full voice clip
             time.sleep(0.3)
             vc.play(audio_source)
